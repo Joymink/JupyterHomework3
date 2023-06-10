b5ffb08180f8f41dec476a75be1763775fc06d5f
Jayden Evans <jayden7evans@gmail.com>
2023-06-10 12:55:18 -0700

    first commit

---
+++ formula.py
@@ -0,0 +1,8 @@
+from math import pi
+
+def squared(length, width):
+    area=abs(length*width)
+    return area
+def circumference(radius):
+    circ= 2*pi*radius
+    return circ
---
+++ module.py
@@ -0,0 +1,2 @@
+def printName(name):
+   print(f"Helloooooooo Mr/Ms {name}...we've been waiting for you!")
